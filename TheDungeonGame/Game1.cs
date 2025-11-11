88a021fdbb5dc5a749beea20816e9d00d749c2c4
SebSutkowy <13sutkowys@student.smchull.org>
2025-11-11 21:02:39 +0000

    Black Bg

--- TheDungeonGame/Game1.cs
+++ TheDungeonGame/Game1.cs
@@ -42,7 +42,7 @@ namespace TheDungeonGame
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
 
