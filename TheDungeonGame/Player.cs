dca0ff05c51a62796984857425749065701ebadc
SebSutkowy <13sutkowys@student.smchull.org>
2025-11-11 21:15:16 +0000

    Created Sprite from which the player class inherits.

---
+++ TheDungeonGame/Player.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace TheDungeonGame
+{
+    internal class Player : Sprite
+    {
+        public Player(Texture2D texture, Vector2 position, float rotation): base(texture, position, rotation)
+        { }
+    }
+}
---
+++ TheDungeonGame/Sprite.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace TheDungeonGame
+{
+    internal class Sprite
+    {
+        public Texture2D Texture { get; set; }
+        public Vector2 Origin => new Vector2(Texture.Width / 2, Texture.Height / 2);
+        public Vector2 Position { get; set; }
+        public float Rotation { get; set; }
+
+        public Sprite(Texture2D texture, Vector2 position, float rotation)
+        {
+            Texture = texture;
+            Position = position;
+            Rotation = rotation;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, null, Color.White, Rotation, Origin, 1.0f, SpriteEffects.None, 0.0f); // will add animations source rectangle will no longer be null
+        }
+    }
+}
