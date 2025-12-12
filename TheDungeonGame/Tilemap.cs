50f386f12cf18739a5073bc4bccb5551c8cde8c0
SebSutkowy <13sutkowys@student.smchull.org>
2025-12-12 13:24:14 +0000

    Created the basic tilemap class

---
+++ TheDungeonGame/Tilemap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace TheDungeonGame
+{
+    public enum TileType 
+    {
+        Wall,
+        Floor
+    }
+
+    public class Tilemap
+    {
+        private Dictionary<string, TileType> Map { get; set; }
+        public Rectangle CameraBounds { get; private set; }
+
+        public Tilemap()
+        {
+            
+        }
+
+        public TileType this[string loc]
+        {
+            get => Map[loc];
+        }
+    }
+}
