b8aab7d26735b37124e1558e12f3c4f226958f19
SebSutkowy <13sutkowys@student.smchull.org>
2026-03-24 11:18:04 +0000

    Hidden fps for debug

--- TheDungeonGame/Game1.cs
+++ TheDungeonGame/Game1.cs
@@ -100,9 +100,11 @@ namespace TheDungeonGame
             SceneManager.Draw();
             UI.Draw();
 
+            /*
             float fps = (float)gameTime.ElapsedGameTime.TotalSeconds;
             fps = 1 / fps;
             Camera.DrawString($"{fps}FPS", new Vector2(0, 50), Color.White);
+            */
 
             _spriteBatch.End();
 
